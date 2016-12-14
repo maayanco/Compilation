01e537e4078bb5563411933c709b8705f567f610
maayan <maayocohen@gmail.com>
2016-12-14 18:11:48 +0200

    many many many changes! big bulk of cse, part 1 missing letrec and mitdefine, problem with let*

--- hw2/compiler.scm
+++ hw2/compiler.scm
@@ -1,10 +1,113 @@
 (include "pattern-matcher.scm")
-          
+
+(define ^quote?
+  (lambda (tag)
+    (lambda (e)
+      (and (pair? e)
+	   (eq? (car e) tag)
+	   (pair? (cdr e))
+	   (null? (cddr e))))))
+
+(define quote? (^quote? 'quote))
+(define unquote? (^quote? 'unquote))
+(define unquote-splicing? (^quote? 'unquote-splicing))
+
 (define const?
+  (let ((simple-sexprs-predicates
+	 (list boolean? char? number? string?)))
+    (lambda (e)
+      (or (ormap (lambda (p?) (p? e))
+		 simple-sexprs-predicates)
+	  (quote? e)))))
+
+(define quotify
+  (lambda (e)
+    (if (or (null? e)
+	    (pair? e)
+	    (symbol? e)
+	    (vector? e))
+	`',e
+	e)))
+
+(define unquotify
+  (lambda (e)
+    (if (quote? e)
+	(cadr e)
+	e)))
+
+(define const-pair?
+  (lambda (e)
+    (and (quote? e)
+	 (pair? (cadr e)))))
+
+(define expand-qq
+  (letrec ((expand-qq
+	    (lambda (e)
+	      (cond ((unquote? e) (cadr e))
+		    ((unquote-splicing? e)
+		     (error 'expand-qq
+		       "unquote-splicing here makes no sense!"))
+		    ((pair? e)
+		     (let ((a (car e))
+			   (b (cdr e)))
+		       (cond ((unquote-splicing? a)
+			      `(append ,(cadr a) ,(expand-qq b)))
+			     ((unquote-splicing? b)
+			      `(cons ,(expand-qq a) ,(cadr b)))
+			     (else `(cons ,(expand-qq a) ,(expand-qq b))))))
+		    ((vector? e) `(list->vector ,(expand-qq (vector->list e))))
+		    ((or (null? e) (symbol? e)) `',e)
+		    (else e))))
+	   (optimize-qq-expansion (lambda (e) (optimizer e (lambda () e))))
+	   (optimizer
+	    (compose-patterns
+	     (pattern-rule
+	      `(append ,(? 'e) '())
+	      (lambda (e) (optimize-qq-expansion e)))
+	     (pattern-rule
+	      `(append ,(? 'c1 const-pair?) (cons ,(? 'c2 const?) ,(? 'e)))
+	      (lambda (c1 c2 e)
+		(let ((c (quotify `(,@(unquotify c1) ,(unquotify c2))))
+		      (e (optimize-qq-expansion e)))
+		  (optimize-qq-expansion `(append ,c ,e)))))
+	     (pattern-rule
+	      `(append ,(? 'c1 const-pair?) ,(? 'c2 const-pair?))
+	      (lambda (c1 c2)
+		(let ((c (quotify (append (unquotify c1) (unquotify c2)))))
+		  c)))
+	     (pattern-rule
+	      `(append ,(? 'e1) ,(? 'e2))
+	      (lambda (e1 e2)
+		(let ((e1 (optimize-qq-expansion e1))
+		      (e2 (optimize-qq-expansion e2)))
+		  `(append ,e1 ,e2))))
+	     (pattern-rule
+	      `(cons ,(? 'c1 const?) (cons ,(? 'c2 const?) ,(? 'e)))
+	      (lambda (c1 c2 e)
+		(let ((c (quotify (list (unquotify c1) (unquotify c2))))
+		      (e (optimize-qq-expansion e)))
+		  (optimize-qq-expansion `(append ,c ,e)))))
+	     (pattern-rule
+	      `(cons ,(? 'e1) ,(? 'e2))
+	      (lambda (e1 e2)
+		(let ((e1 (optimize-qq-expansion e1))
+		      (e2 (optimize-qq-expansion e2)))
+		  (if (and (const? e1) (const? e2))
+		      (quotify (cons (unquotify e1) (unquotify e2)))
+		      `(cons ,e1 ,e2))))))))
+    (lambda (e)
+      (optimize-qq-expansion
+       (expand-qq e)))))
+
+(define not-empty?
+  (lambda (lst)
+    (if (equal? lst `()) #f #t)))
+
+(define const2?
   (lambda (val)
     (or
      (equal? val `,(void))
-     (equal? val ''())
+     (equal? val '())
      (vector? val)
      (boolean? val)
      (char? val)
@@ -12,7 +115,7 @@
      (string? val)
      )))
 
-(define quote?
+(define quote1?
   (lambda (val)
     (if (list? val)
         (if (equal? (car val) 'quote) #t #f)
@@ -82,12 +185,6 @@
          (lambda (exp)
          (and (list? exp) (equal? (car exp) 'seq))))
 
-(define beginify
-	(lambda (s)
-		(cond
-			((null? s) *void-object*)
-			((null? (cdr s)) (cdr s))
-			(else `(begin ,@s)))))
 
 (define nested-if ;;for and macro expansion
   (lambda (exprs)
@@ -102,16 +199,18 @@
            `(if ,(caar exprs) ,@(cdar exprs) ,(nested-if-cond (cdr exprs))))
            ((and (not (equal? exprs '())) (equal? (caar exprs) 'else)) (cadar exprs)))
     ))
-          
 
+(define beginify
+  (lambda (expr)
+    (fold-left (lambda (l r)  (if (and (list? r) (equal? `begin (car r)))  (append l (beginify (cdr r))) `(,@l ,r))) '() expr)))
 
 (define parse-2
   (let ((run
          (compose-patterns
           (pattern-rule ;;const
-           (? 'c const?) (lambda (c) `(const ,c)))
+           (? 'c const2?) (lambda (c) `(const ,c)))
           (pattern-rule ;;const - quote
-           (? 'c quote?) (lambda (c) `(const ,@(cdr c))))
+           (? 'c quote1?) (lambda (c) `(const ,@(cdr c))))
           (pattern-rule ;; if test dit dif
            `(if  ,(? 'test) ,(? 'dit) ,(? 'dif))
            (lambda (<test> <dit> <dif>)
@@ -136,7 +235,11 @@
                  (expand-lambda identified-lambda exprs)
                  (expand-lambda identified-lambda (cons 'begin (cons exprs exprs-lst)))
              ))))
-            ; `(lambda ,args ,exprs))) 
+            (pattern-rule ;;quasi-quote
+             `(quasiquote . ,(? 'args))
+             (lambda (args)
+               ;(display (car args)) (display "||\n")
+               (parse-2 (expand-qq (car args)))))
            (pattern-rule ;;define MIT
             `(define ( ,(? `var1) . ,(? `vars-lst) ) ,(? `exp1) . ,(? `exps-lst))
             (lambda (var1 vars-lst exp1 exps-lst)
@@ -150,18 +253,15 @@
            (pattern-rule ;;set!
             `(set! ,(? `var) ,(? `exp))
             (lambda (var exp) `(set (var ,var) ,(parse-2 exp) )))
-           (pattern-rule ;;sequence
-            `(begin ,(? 'val1) . ,(? `vals-lst))
-            (lambda (val1 vals-lst)
-              `(seq ( ,(parse-2 val1)
-                      ,@(map
-                         
-                      (lambda (item) (let ((parsed-item (parse-2 item)))
-                                       (if seq? parsed-item)
-                                       ((lambda ()
-                                          (cadr parsed-item)))
-                                       parsed-item))
-                      vals-lst)))))
+           (pattern-rule
+            `(begin) (lambda () (parse-2 ``())))
+           (pattern-rule 
+            `(begin  ,(? `expr))
+            (lambda (expr) (parse-2 expr)))
+           (pattern-rule 
+            `(begin . ,(? `lst list?)) 
+            (lambda (lst) `(seq ,(map parse-2 (beginify lst)))))
+
            (pattern-rule ;;and
             `(and)
             (lambda () (parse-2 #t)))
@@ -176,19 +276,20 @@
             (lambda (expr exprs-lst)
               (let 
                   ((new-exprs (map (lambda (item) (if (and (list? (cdr item)) (not (equal? (length (cdr item)) 1)))
-                                       ; (begin ;(display (list (car item) (cons `begin (cdr item))))
-                                              ; (display "||\n")
                                                (list (car item) (cons `begin (cdr item)))
                                                item))
                                       (cons expr exprs-lst))))
-               ; (display "exprs:") (display new-exprs) (display "end-exprs")
-            ;    (map (lambda (item) (display "{") (display item) (display "}\n")) exprs)
                 (parse-2 (nested-if-cond new-exprs))
                 )))
            (pattern-rule ;;let
             `(let () ,(? `exprs-lst))
             (lambda (exprs-lst)
               (parse-2 `((lambda () ,exprs-lst)))))
+          ; (pattern-rule
+          ;  `(let ((,(? `var)  ,(? `val))) . ,(? `rest))
+          ;  (lambda (var val)
+          ;    ))
+           
            (pattern-rule
             `(let ((,(? 'var var?) ,(? `val)) . ,(? 'rest)) . ,(? 'exprs))
             (lambda (var val rest exprs)
@@ -199,17 +300,25 @@
 		(pattern-rule ;;let*
                 `(let* () ,(? 'expr) . ,(? 'exprs list?))
                 (lambda (expr exprs)
-                  (parse-2 (cons 'begin (cons expr exprs)))))
+                  (if (not-empty? exprs)
+                      (parse-2 `(let () ,(cons expr exprs)))
+                      (parse-2 `(let () ,expr)))
+                  ))
 		(pattern-rule
                  `(let* ((,(? 'var) ,(? `val)) . ,(? 'rest)) . ,(? 'exprs))
-                 (lambda (var val rest exprs) (parse `(let ((,var val)) (let* ,rest . ,exprs)))))
+                 (lambda (var val rest exprs)
+                   (if (and (equal? (length rest) 0) (equal? (length exprs) 1))
+                        (parse-2 `(let ((,var ,val)) ,(car exprs)))
+                        (parse-2 `(let ((,var ,val)) (let* ,rest ,(car exprs)))))
+                   ))
            (pattern-rule ;;application
             `( ,(? `var not-is-reserved?) . ,(? `vars-lst))
             (lambda (var vars-lst) `(applic ,(parse-2 var) ,(map parse-2 vars-lst))))
            
           (pattern-rule
            (? 'v var?) (lambda (v) `(var ,v)))
-          
+
+
           
           )))
          (lambda (e) (run e
---
+++ hw2/cse.scm
@@ -0,0 +1,148 @@
+
+;; Taken from my ppl Assignment 3 question 1ab that i wrote!!
+
+(define flatmap
+  (lambda (proc seq)
+    (foldr append (list) (map proc seq))))
+
+(define permutations
+  (lambda (s)
+    (if (set-empty? s)
+        (list empty)
+        (flatmap
+         (lambda (x)
+           (map (lambda (p) (cons x p))
+                (permutations (set-remove-el x s))))
+         (set->list s)))
+    ))
+
+(define empty?
+  (lambda (a) (if (equal? a `()) #t #f)))
+               
+(define powerset
+  (lambda (s)
+    (if (empty? s)
+        `(())
+        (fold-right append '()
+               (map (lambda (item) (let ((newElement (car s))) (list item (cons newElement item)))) (powerset (cdr s)))))))
+
+(define is-smallest?
+  (lambda (expr)
+    (if (not (list? expr))
+        #f
+        (andmap (lambda (item) (if (not (list? item)) #t #f)) expr))))
+
+(define not-empty?
+  (lambda (a) (if (equal? a '()) #f #t)))
+
+(define saved-vars '())
+
+(define go-over-and-replace
+  (lambda (exp item-to-replace gensym-name)
+    (map
+     (lambda (item) (cond ((and (list? item) (not-empty? item) (is-smallest? item)) ;;we found the smallest element - we will just disp
+                               (if (equal? item item-to-replace) gensym-name item))
+                              
+                              ((and (list? item) (not-empty? item)) ;;item is a subexpression! we need to scan it as well...
+                               ( go-over-and-replace item item-to-replace gensym-name))
+                              
+                              ((or (not (list? item)) (empty? item)) ;;item is not a list or is an empty list return it as it is..
+                               (if (equal? item item-to-replace)
+                                   gensym-name
+                                   item))
+                              ))
+     exp)))
+
+
+(define get-occurrences-num
+  (lambda (exp item-to-find)
+    (cond
+      ((or (not (list? exp)) (equal? exp `())) 0)
+      ((list? exp)
+       (if (equal? (car exp) item-to-find)
+           (+ 1 (get-occurrences-num (car exp) item-to-find) (get-occurrences-num (cdr exp) item-to-find))
+           (+ 0 (get-occurrences-num (car exp) item-to-find) (get-occurrences-num (cdr exp) item-to-find))))
+           )))
+    
+(define update-saved-vars
+  (lambda (lst)
+    (set! saved-vars (cons lst saved-vars))))
+
+
+(define go-over-exp
+  (lambda (exp original-exp)
+          (if (or (not (list? exp)) (not (not-empty? exp)))
+              (if (equal? exp `())
+                  (begin ;(display "\n i'm super `() out")
+                    original-exp)
+                  (begin ;(display "\n im out") (display exp)
+                         exp))
+
+              (begin
+              ;      (display "\n i'm in!!")
+                  ;;all the rest of this lambda happens only if exp is a list and not empty..
+                  (if (and (is-smallest? exp) (> (get-occurrences-num original-exp exp) 1)) ;; we have found a good expression
+                      ;;then clause
+                      (begin   
+                               (let* ((gensym-name (symbol->string (gensym)))
+                                      (replaced-exp (go-over-and-replace original-exp exp gensym-name)))
+                                 (update-saved-vars (list gensym-name exp))
+                                 replaced-exp))
+                      
+                      ;;we haven't found a recurring smallest, we will search inside the car or in the cdr
+                      (begin 
+                      (let* ((ret-car-expr (go-over-exp (car exp) original-exp)))
+                         (if (equal? ret-car-expr (car exp))
+                             ;;then : nothing changed, we need to search in the cdr
+                             (go-over-exp (cdr exp) original-exp)
+                             ;;else - it did change! we will return the changed exp
+                              ret-car-expr))))))))
+                             
+
+;(define go-over-exp
+;  (lambda (exp)
+;    (cond ((and (list? exp) (not-empty? exp) (is-smallest? (car exp))) (display "in1") ;;we have found a smallest item!!
+;           (if (> (get-occurrences-num exp (car exp)) 1) ;;this smallest item appears more than once!!
+;               ;;then clause
+;               (begin   (let* ((gensym-name (gensym))
+;                      (replaced-exp (go-over-and-replace exp (car exp) gensym-name)))
+;                 (update-saved-vars (list gensym-name (car exp)))
+;                 replaced-exp))
+;               ;;else clause:
+;               (begin  (car exp))))
+;          ((and (list? exp) (not-empty? exp) (list? (car exp)) (not (is-smallest? (car exp)))) (display "in2") (go-over-exp (car exp))) ;; search for smallest item inside the first item of this exp
+          
+;          ((and (list? exp) (not-empty? exp) (not-empty? (cdr exp))) (display "in3") (cons (car exp) (go-over-exp (cdr exp)))) ;; we want to progress to the cdr of this exp and maybe i'll find the smallest item there..
+;          )))
+
+(define counter 0)
+
+(define apply-goe-rec
+  (lambda (exp original-exp)
+    (set! counter (+ counter 1))
+    (display "\n\n") (display "current exp:") (display exp) (display "\n")
+    (if (equal? counter 8)
+        exp
+    (let ((new-exp (go-over-exp exp original-exp)))
+          (if (equal? new-exp exp)
+              ;;if nothing changed in this iteration than that's it! we return new-exp
+              (begin (display "hello\n") new-exp)
+              ;;else clause- if the expression did change, we want to
+              (begin (display "changed! go again: ") (display " before: ") (display exp) (display " after: ") (display new-exp) (display " \n")
+                     (apply-goe-rec new-exp new-exp)))))))
+    
+    
+(define cse
+  (lambda (expr) 
+    (map
+     (lambda (item) (cond ((and (list? item) (not-empty? item) (is-smallest? item))
+                           ; now we should go over the rest!! of the elements and search if there is an element equal to item
+                           ;; if we end the search and expr isn't changed - than we need to finish and conclude that our expression is the same
+                           1)
+                           ((and (list? item) (not-empty? item) (not (is-smallest? item))) ; now we should go deeper inside until we find the smallest!
+                            2)
+                           (else (display "item:") (display item) (display "\n") item)
+                           ))
+     expr)
+    ))
+    
